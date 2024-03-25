5ab95fef53ddcf08048b3471930b03d1cb922a43
Vigneshkumar <vvadive2@ford.com>
2024-03-25 21:54:39 +0530

    Initial Commit

---
+++ src/main/java/com/jenkins/test/myjenkins/MyjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.test.myjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/test/myjenkins/MyjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.test.myjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
